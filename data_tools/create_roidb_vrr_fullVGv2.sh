90e4150b011e8f7b1292823e04a36f43da0f50af
Kuang-Huei Lee <khlee@Kuang-Hueis-MacBook-Pro.local>
2019-03-12 14:19:51 -0700

    use coco split

---
+++ data_tools/create_roidb_vrr_fullVGv2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+set -e
+
+OUT_PATH="."
+
+# N_OBJ=150 # number of object categories
+# N_REL=50 # number of relationship categories
+
+H5=VG-SGG.h5
+JSON=VG-SGG-dicts.json
+FRAC=1
+IMDB=imdb_1024.h5
+
+python vg_to_roidb_vrr_fullVG.py \
+    --imdb $IMDB \
+    --json_file $OUT_PATH/$JSON \
+    --h5_file $OUT_PATH/$H5 \
+    --load_frac $FRAC \
+    --min_box_area_frac 0.002 \
+    # --train_frac 0.95 \
+    # --val_frac 0.95 \
+    # --num_objects $N_OBJ \
+    # --num_predicates $N_REL \
