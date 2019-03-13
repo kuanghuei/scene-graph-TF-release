0e2eb7d3d3875a0c965e8093a7acf437879fe576
Kuang-Huei Lee <khlee@Kuang-Hueis-MacBook-Pro.local>
2019-03-12 17:09:02 -0700

    check

---
+++ data_tools/create_roidb_vrr_oldv3_coco_aware.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+set -e
+
+OUT_PATH="."
+
+# N_OBJ=150 # number of object categories
+# N_REL=50 # number of relationship categories
+
+H5=VG-SGG.h5
+JSON=VG-SGG-dicts.json
+FRAC=1
+IMDB=imdb_1024.h5
+
+python vg_to_roidb_vrr_oldv3_coco_aware.py \
+    --imdb $IMDB \
+    --json_file $OUT_PATH/$JSON \
+    --h5_file $OUT_PATH/$H5 \
+    --load_frac $FRAC \
+    --min_box_area_frac 0.002 \
+    --external_VG_SGG_dicts vrr_meta_v31/VG-SGG-dicts.json \
+    # --num_objects $N_OBJ \
+    # --num_predicates $N_REL \
